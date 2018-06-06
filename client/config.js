ef6eaa31cfd8b29fa4874e501810513d34a7291b
Md Mansoor Kooripparambil <mdmansoor.k@maybank.com>
2018-06-06 15:53:37 +0800

    url change

--- client/config.js
+++ client/config.js
@@ -1,7 +1,7 @@
 app.factory('Config', function () {
     var root = {};
 
-    root.server_url = 'https://webserver-webserver.193b.starter-ca-central-1.openshiftapps.com/';
+    root.server_url = 'https://webserver-webserver.193b.starter-ca-central-1.openshiftapps.com:8080/';
     // root.server_url = 'http://localhost:8080/';
 
     return root;
